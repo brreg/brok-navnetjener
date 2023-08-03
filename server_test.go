c93e7b5b17f75f3cc55e71d189c06fb55f4d4e2b
Øyvind Hatland <oha@brreg.no>
2023-08-03 10:23:22 +0200

    test creation and lookup

--- server_test.go
+++ server_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"brok/navnetjener/model"
 	"bytes"
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -33,17 +35,24 @@ func TestWalletCreation(t *testing.T) {
 	router := setup()
 
 	w := httptest.NewRecorder()
-	jsonBody := []byte(`{
-	"first_name": "Kari",
-	"last_name": "Norman",
-	"sosial_security_number": "01019844422",
-	"wallet_address":` + randomWalletAddress() + `
-	}`)
-	req, _ := http.NewRequest("POST", "/wallet", bytes.NewBuffer(jsonBody))
-	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	testWallet := model.Wallet{
+		FirstName:            "Kari",
+		LastName:             "Norman",
+		SosialSecurityNumber: "01019844422",
+		WalletAddress:        randomWalletAddress(),
+	}
+	json, _ := json.Marshal(testWallet)
+	req, _ := http.NewRequest("POST", "/wallet", bytes.NewReader(json))
+	req.Header.Set("Content-Type", "application/json")
+
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusCreated, w.Code)
+
+	storedWallet, _ := model.FindWalletByAddress(testWallet.WalletAddress)
+
+	assert.Equal(t, storedWallet.WalletAddress, testWallet.WalletAddress)
+
 }
 
 func TestWalletAddressDoesNotExist(t *testing.T) {
