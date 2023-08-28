58d581853ad40e4008a6d1feb3d9b2b78afbf952
Øyvind Hatland <oha@brreg.no>
2023-08-28 14:44:45 +0200

    find 25 captable using pagination

--- api/foretak.go
+++ api/foretak.go
@@ -3,6 +3,7 @@ package api
 import (
 	"brok/navnetjener/model"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,9 +20,38 @@ func GetForetakByOrgnr(context *gin.Context) {
 	// check if foretak is empty
 
 	if capTable.Name == "" {
-		context.JSON(http.StatusNotFound, gin.H{"error": "finner ikke foretak med orgnr " + orgnr})
+		context.JSON(http.StatusNotFound, gin.H{"error": "finner ikke Aksjebok for orgnr " + orgnr})
 		return
 	}
 
 	context.JSON(http.StatusOK, capTable)
+}
+
+// GetForetak returns 25 foretak from TheGraph and the database
+// user can use the queryparameter "page" to paginate
+func GetForetak(context *gin.Context) {
+	page := context.Query("page")
+	safePage := model.SanitizeString(page)
+
+	// convert page to int
+	// if page is empty, set it to 0
+	// if page is not a number, set it to 0
+	// if page is less than 0, set it to 0
+	if safePage == "" {
+		safePage = "0"
+	}
+
+	safePageInt, err := strconv.Atoi(safePage)
+
+	if err != nil {
+		safePageInt = 0
+	}
+
+	capTables, err := model.FindForetak(safePageInt)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, capTables)
 }
