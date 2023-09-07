079c27542c64492a277a54a3406f24a3faa40734
Øyvind Hatland <oha@brreg.no>
2023-09-07 17:10:02 +0200

    switched to go-sanitize package

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/bxcodec/faker/v3 v3.8.1
 	github.com/gin-gonic/gin v1.9.1
 	github.com/joho/godotenv v1.5.1
-	github.com/microcosm-cc/bluemonday v1.0.25
+	github.com/mrz1836/go-sanitize v1.3.1
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.8.4
 	gorm.io/driver/postgres v1.5.2
@@ -14,7 +14,6 @@ require (
 )
 
 require (
-	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -24,7 +23,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/gorilla/css v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.2 // indirect
--- model/sanitazion.go
+++ model/sanitazion.go
@@ -1,53 +1,22 @@
 package model
 
 import (
-	"errors"
-	"regexp"
-
-	"github.com/microcosm-cc/bluemonday"
+	"github.com/mrz1836/go-sanitize"
 )
 
+// Only allow AlphaNumeric character's to return
 func SanitizeString(input string) string {
-	/*
-		Do not allow any HTML tags.
-		When content is passed through this policy, all HTML is stripped away,
-		leaving only the plain text content.
-		Even safe HTML content will be stripped out.
-		This ensures that no potentially harmful content can be embedded within the sanitized text
-	*/
-	p := bluemonday.StrictPolicy()
-	return p.Sanitize(input)
+	return sanitize.AlphaNumeric(input, true)
 }
 
-func SanitizeWallet(w *Wallet) error {
-	p := bluemonday.StrictPolicy()
+func SanitizeWallet(w *Wallet) {
 
 	// Sanitize string fields
-	w.FirstName = p.Sanitize(w.FirstName)
-	w.LastName = p.Sanitize(w.LastName)
-	w.Pnr = p.Sanitize(w.Pnr)
-	w.BirthDate = p.Sanitize(w.BirthDate)
-	w.WalletAddress = p.Sanitize(w.WalletAddress)
-
-	// Validate Ethereum address
-	if err := validateEthereumAddress(w.WalletAddress); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func validateEthereumAddress(address string) error {
-	// Check for length and starting characters
-	if len(address) != 42 || address[:2] != "0x" {
-		return errors.New("invalid wallet address format")
-	}
-
-	// Ensure the remaining characters are hexadecimal
-	matched, err := regexp.MatchString(`^0x[a-fA-F0-9]{40}$`, address)
-	if err != nil || !matched {
-		return errors.New("invalid wallet address format")
-	}
-
-	return nil
+	w.OwnerPersonFirstName = SanitizeString(w.OwnerPersonFirstName)
+	w.OwnerPersonLastName = SanitizeString(w.OwnerPersonLastName)
+	w.OwnerPersonPnr = SanitizeString(w.OwnerPersonPnr)
+	w.OwnerPersonBirthDate = SanitizeString(w.OwnerPersonBirthDate)
+	w.OwnerCompanyName = SanitizeString(w.OwnerCompanyName)
+	w.OwnerCompanyOrgnr = SanitizeString(w.OwnerCompanyOrgnr)
+	w.WalletAddress = SanitizeString(w.WalletAddress)
 }
