6fad16581814ee993f087aa81eba56bde76a4916
Øyvind Hatland <oha@brreg.no>
2023-10-09 11:13:06 +0200

    move HTTP GET /health outside v1

--- server.go
+++ server.go
@@ -17,6 +17,8 @@ func main() {
 
 func apiRoutes(router *gin.Engine) {
 
+	router.GET("/health/", api.Health)
+
 	// Group API endpoints in /v1
 	v1 := router.Group("/v1")
 
@@ -30,7 +32,6 @@ func apiRoutes(router *gin.Engine) {
 	v1.GET("/aksjebok/:orgnr/balanse/:id", api.GetNumberOfSharesForOwnerOfAForetak)
 	v1.POST("/aksjebok/:orgnr/aksjeeier", api.GetOwnersForForetak)
 
-	v1.GET("/health/", api.Health)
 }
 
 func serveApplication(router *gin.Engine) {
